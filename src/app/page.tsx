44b8a72aa2b49f05a1d4d4889fa97cc7dbf457b3
ibrar <ibrariqbal93@gmail.com>
2025-09-09 18:36:53 +0500

    Some changes made in our team tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // import Image from "next/image";
 
-<<<<<<< HEAD
 import Homepage from "./home/page";
 
  
@@ -9,19 +8,5 @@ export default function Home() {
     <>
           <Homepage/>
     </>
-
-      
-
-    
-=======
-import Typography from "@/components/UI/Typography";
-import { products1 } from "@/components/UI/home/hero";
-
-export default function Home() {
-  return (
-    <div className="">
-
-    </div>
->>>>>>> 1f9eef40f942c60c46671d86a255eea21012b858
   );
 }
