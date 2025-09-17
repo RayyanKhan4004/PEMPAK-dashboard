1460f638ab81ee0fe3c87d2561b570cd3121d29f
RayyanKhan4004 <rk4004104@gmail.com>
2025-09-17 16:36:58 +0500

    fixed navbar dropdown logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,113 @@
 // components/Navbar.tsx
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 import Typography from "@/components/UI/Typography";
-
+import { useEffect, useState , useRef } from "react";
 export const Navbar = () => {
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const navLinks = [
+    { label: "Home", link: "/home" },
+    { label: "About", link: "/about" },
+
+    {
+      label: "Our Products",
+      catagoried: [
+        {
+          name: "Switchgear / Controlgear",
+          sub: [{ name: "Low Voltage Switchgear" }],
+        },
+        {
+          name: "Power Distribution Transformer",
+        },
+        {
+          name: "Green Energy",
+        },
+        {
+          name: "Appliances",
+        },
+      ],
+    },
+    { label: "Blog", link: "/blog" },
+    { label: "Team", link: "/our-team" },
+    { label: "Services", link: "/services" },
+    { label: " Contact us", link: "/contact" },
+  ];
+  const [mainDorpdown, setMainDropdown] = useState("");
+  const [subDropdown, setSubDropdown] = useState("");
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+        if (
+          dropdownRef.current &&
+          !dropdownRef.current.contains(event.target as Node)
+        ) {
+          setMainDropdown("");
+          setSubDropdown("");
+        }
+      };
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [dropdownRef]);
+  // useEffect(() => {});
   return (
     <nav className="shadow-md p-4 flex items-center justify-between">
       <Typography variant="h3" weight="b" color="primary" className="ml-17">
-  PEMPAK
-</Typography>
+        PEMPAK
+      </Typography>
 
       {/* Navigation Links */}
-      <div className="flex space-x-6 text-lg font-medium items-center">
-        <Link href="/home" className="hover:text-amber-600 transition-colors">Home</Link>
-        <Link href="/about" className="hover:text-amber-600 transition-colors">About</Link>
-        <Link href="/products" className="hover:text-amber-600 transition-colors">Our Products</Link>
-        <Link href="/services" className="hover:text-amber-600 transition-colors">Services</Link>
-        <Link href="/our-team" className="hover:text-amber-600 transition-colors">Our Team</Link>
-        <Link href="/blog" className="hover:text-amber-600 transition-colors">Blog</Link>
-        <Link
-          href="/contact"
-          className="hover:text-white text-white bg-orange-600 px-6 py-2 rounded-xl transition-colors"
-        >
-          Contact us
-        </Link>
+      <div className="flex relative space-x-6 text-lg font-medium items-center">
+        {navLinks.map((item, index) => {
+          return (
+            <Link
+              href={item.link ? item.link : ("/" as string)}
+              className="hover:font-bold"
+              onClick={() => setMainDropdown(item.label)}
+              // onMouseLeave={() => setMainDropdown("")}
+            >
+              {item.label}
+
+              {mainDorpdown == item.label && item.catagoried && (
+                <div ref={dropdownRef} className=" absolute top-8 left-1/2 -translate-x-1/2 bg-white shadow-lg space-y-2 rounded-[12px] z-10">
+                  {item.catagoried.map((sub, i) => {
+                    return (
+                      <div
+                        className="reletive px-4"
+                        onClick={() => setSubDropdown(sub.name)}
+                        // onMouseLeave={() => setSubDropdown("")}
+                      >
+                        <Typography
+                          weight="semi-b"
+                          varient="ps"
+                          className=" hover:text-white hover:bg-[var(--color-primary)]"
+                        >
+                          {sub.name}
+                        </Typography>
+
+                        {subDropdown === sub.name && sub.sub && (
+                          <div className="absolute left-full top-0 bg-white shadow-lg   space-y-2 rounded-[12px] z-10 hover:text-white hover:bg-[var(--color-primary)]">
+                            {sub.sub.map((s, j) => {
+                              return (
+                                <div className="p-4 bg-white rounded-[12px]">
+                                  <p className="  hover:text-white hover:bg-[var(--color-primary)] text-nowrap">
+                                    {s.name}
+                                  </p>
+                                </div>
+                              );
+                            })}
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </Link>
+          );
+        })}
+      
       </div>
     </nav>
   );
